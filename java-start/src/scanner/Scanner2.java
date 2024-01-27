30b3ed3126e6b0befa2bafd4c6704a7bdb8cc676
k-cielo <skyhn10@gmail.com>
2024-01-27 23:08:09 +0900

    ScannerStudyCommit

--- java-start/src/scanner/Scanner2.java
+++ java-start/src/scanner/Scanner2.java
@@ -13,7 +13,7 @@ public class Scanner2 {
         System.out.print("두 번째 숫자를 입력하세요:");
         int num2 = scanner.nextInt();
 
-        int sum = num1 + num2;git
+        int sum = num1 + num2;
         System.out.println("두 숫자의 합:"+sum);
 
 
