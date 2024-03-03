da0c19217b1828dc29a141b2afa4b18398d31c18
k-cielo <skyhn10@gmail.com>
2024-03-03 22:47:49 +0900

    extendsCommit

---
+++ java-basic/src/extends1/ex3/Car.java
@@ -0,0 +1,15 @@
+package extends1.ex3;
+
+public class Car {
+    public void move(){
+        System.out.println("차를 이동합니다.");
+    }
+
+
+
+    //기능추가(부모에만 추가하면 자식은 다 물려받게됨)
+    public void openDoor(){
+        System.out.println("문을 엽니다.");
+    }
+
+}
