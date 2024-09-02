bbd70727adbe4b5259be1bd5977892bb6e82f450
k-cielo <skyhn10@gmail.com>
2024-09-02 23:59:59 +0900

    generic2

--- java-mid2/src/generic/ex3/AnimalHospitalMainV1.java
+++ java-mid2/src/generic/ex3/AnimalHospitalMainV1.java
@@ -1,12 +1,13 @@
 package generic.ex3;
 
+import generic.animal.Animal;
 import generic.animal.Cat;
 import generic.animal.Dog;
 
-public class AnimalHospitalMainV0 {
+public class AnimalHospitalMainV1 {
     public static void main(String[] args) {
-        DogHospital dogHospital = new DogHospital();
-        CatHospital catHospital = new CatHospital();
+        AnimalHospitalV1 dogHospital = new AnimalHospitalV1();
+        AnimalHospitalV1 catHospital = new AnimalHospitalV1();
 
 
         Dog dog = new Dog("멍멍이1", 100);
@@ -21,12 +22,12 @@ public class AnimalHospitalMainV0 {
         catHospital.checkup();
 
         //문제1: 개 병원에 고양이 전달
-//        dogHospital.set(cat); // 다른 타입 입력: 컴파일 오류
+        dogHospital.set(cat); // 매개변수 체크 실패: 컴파일 오류가 발생하지 않음
 
         //문제2: 개타입 반환
-        Dog biggerDog= dogHospital.bigger(new Dog("멍멍이2", 200));
+        Animal animal= dogHospital.bigger(new Dog("멍멍이2", 200));
         System.out.println("biggerDog = "+biggerDog);
 
     }
+
 }
- 
