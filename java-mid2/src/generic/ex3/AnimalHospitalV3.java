eaa1c899528d9a8faa262ea2e309b0b890a9cefc
k-cielo <skyhn10@gmail.com>
2024-09-04 23:37:22 +0900

    generic2

--- java-mid2/src/generic/ex3/AnimalHospitalV3.java
+++ java-mid2/src/generic/ex3/AnimalHospitalV3.java
@@ -1,22 +1,26 @@
 package generic.ex3;
 
+
 import generic.animal.Animal;
 
-public class AnimalHospitalV1 {
+public class AnimalHospitalV3<T extends Animal> {
 
-    private Animal animal;
+    private T animal;//T에 들어올 수 있는 타입을 제한함
 
-    public void set(Animal animal) {
+    public void set(T animal) {
         this.animal = animal;
     }
 
     public void checkup(){
+
         System.out.println("동물 이름:" + animal.getName());
         System.out.println("동물 크기:" + animal.getSize());
         animal.sound();
     }
 
-    public Animal bigger(Animal target) {
+    public T bigger(T target) {
+
         return animal.getSize() > target.getSize() ? animal : target;
+
     }
 }
